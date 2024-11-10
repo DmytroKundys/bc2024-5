5eadfc2deeed6c7f9a93ff10f1994b3a69b1a835
Dmytro bc2024-5 <dkundys2.0@gmail.com>
2024-11-10 13:52:56 +0200

    Додано обробку параметрів командного рядка та запуск сервера

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
-'use strict';
+const { Command } = require('commander');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
-console.log('Hello world');
+const program = new Command();
+program
+    .requiredOption('-h, --host <type>', 'адреса сервера')
+    .requiredOption('-p, --port <type>', 'порт сервера')
+    .requiredOption('-c, --cache <type>', 'шлях до кешу');
+
+program.parse(process.argv);
+const options = program.opts();
+
+const app = express();
+const cacheDir = path.resolve(options.cache);
+
+// Перевірка існування директорії кешу
+if (!fs.existsSync(cacheDir)) {
+    fs.mkdirSync(cacheDir, { recursive: true });
+}
+
+app.listen(options.port, options.host, () => {
+    console.log(`Сервер працює на http://${options.host}:${options.port}`);
+});
